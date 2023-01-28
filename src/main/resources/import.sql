2c8126b4ee3df130ea867173a9bcb94a5890498a
matheusktt <matheus_0119@outlook.com>
2023-01-27 22:12:32 -0300

    seeding database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Matheus Oliveira', '54300478131', 6000.0, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos Eduardo', '14052378841', 5500.0, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Daniel Carvalho', '95416842271', 9500.0, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jorge Pereira', '07011863429', 7000.0, NOW(), 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fred Ramires', '95417484837', 500.0, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduardo Rodrigues', '48847593145', 1000.0, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Lucca Andrade', '14056914798', 1900.0, NOW(), 0);
