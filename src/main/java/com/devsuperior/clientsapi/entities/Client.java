b194f29b2b8f2c1e64651c45ce17d82b98267051
matheusktt <matheus_0119@outlook.com>
2023-01-27 22:12:06 -0300

    updating strategy

--- src/main/java/com/devsuperior/clientsapi/entities/Client.java
+++ src/main/java/com/devsuperior/clientsapi/entities/Client.java
@@ -12,7 +12,7 @@ public class Client implements Serializable {
     private static final long seriaLVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
