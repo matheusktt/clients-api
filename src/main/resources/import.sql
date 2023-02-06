2c4dc9ae8de81a59d9178611037b9603f45a4303
matheusktt <matheus_0119@outlook.com>
2023-02-06 12:52:47 -0300

    updating database seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Matheus Oliveira', '54300478131', 6000.0, NOW(), 2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos Eduardo', '14052378841', 5500.0, NOW(), 1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Daniel Carvalho', '95416842271', 9500.0, NOW(), 1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jorge Pereira', '07011863429', 7000.0, NOW(), 4);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fred Ramires', '95417484837', 500.0, NOW(), 0);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduardo Rodrigues', '48847593145', 1000.0, NOW(), 2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Lucca Andrade', '14056914798', 1900.0, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Matheus Oliveira', '54300478131', 6000.0, '2000-01-19T08:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos Eduardo', '14052378841', 5500.0, '1997-03-29T04:10:40Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Daniel Carvalho', '95416842271', 9500.0, '1991-11-22T09:42:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jorge Pereira', '07011863429', 7000.0, '1995-07-04T12:43:23Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fred Ramires', '95417484837', 3500.0, '1996-08-08T11:36:18Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduardo Rodrigues', '48847593145', 1000.0, '2002-04-14T03:14:40Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Lucca Andrade', '14056914798', 1900.0, '2001-09-07T07:47:32Z', 0);
